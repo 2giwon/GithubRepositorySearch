e43cbc4a28c6ef2fde1dc12069e13dc64aafd82a
2giwon <2giwon@gmail.com>
2020-11-08 21:48:32 +0900

    [수정] - MainActivity dataBinding 처리 및 검색 쿼리 입력 처리

--- app/src/main/java/kr/eg/egiwon/githubrepositorysearch/main/MainActivity.kt
+++ app/src/main/java/kr/eg/egiwon/githubrepositorysearch/main/MainActivity.kt
@@ -1,8 +1,28 @@
 package kr.eg.egiwon.githubrepositorysearch.main
 
+import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import dagger.hilt.android.AndroidEntryPoint
 import kr.eg.egiwon.githubrepositorysearch.R
+import kr.eg.egiwon.githubrepositorysearch.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        binding.buttonSearch.setOnClickListener {
+            viewModel.getRepository(binding.editSearch.text.toString())
+        }
+    }
+}
