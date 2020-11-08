5fa4168209a591466328aab69fd955ee19848d7b
middlefgr <2giwon@gmail.com>
2020-11-08 21:21:45 +0900

    [수정] - MainActivity 패키지 변경

---
+++ app/src/main/java/kr/eg/egiwon/githubrepositorysearch/main/MainActivity.kt
@@ -0,0 +1,8 @@
+package kr.eg.egiwon.githubrepositorysearch.main
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kr.eg.egiwon.githubrepositorysearch.R
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main)
